4be9b02c93beab7912e3301ec08eb5f6385a1099
Mohit Chandana <chandanamohit@gmail.com>
2020-04-29 01:31:50 +0530

    Complete tutorial code for Vue Js Blogs project with basic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//create global filer
+Vue.filter('snippet', val => {
+  if(!val || typeof(val) != 'string') return ''
+  val = val.slice(0,50) + '...'
+  console.log(val.length)
+  return val
+})
+
 Vue.config.productionTip = false
 
 new Vue({
